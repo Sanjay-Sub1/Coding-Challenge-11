9c4fc21f33804a7e42badcdacd793109ba2bdecb
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-27 14:08:19 -0400

    Calculate and update total price dynamically based on product and quantity

--- order-management-system.js
+++ order-management-system.js
@@ -1,14 +1,18 @@
+//Function to calculate the total price
 function calculateTotal(){
     const productDopdown = document.getElementById('product');
     const quantityInput = document.getElementById('quantity');
     const totalPriceDisplay = document.getElementById('totalPrice');
 
+//Changes the html inputs into both a float point and an integer
 const priceOfProduct = parseFloat(productDopdown.value);
 const quantity = parseInt(quantityInput.value);
 
+//mulitplies the quantity by price to get total amount
 const totalPrice = priceOfProduct * quantity;
 totalPriceDisplay.textContent = totalPrice.toFixed(2);
 }
 
+//Dynamically updates the page
 document.getElementById('product').addEventListener('change',calculateTotal);
 document.getElementById('quantity').addEventListener('input', calculateTotal);
