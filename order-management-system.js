58d7f768d17b4764f65cf4ae58a9b3e9920eaaee
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-27 14:15:30 -0400

    Handle order submission and display order summary

--- order-management-system.js
+++ order-management-system.js
@@ -12,7 +12,21 @@ const quantity = parseInt(quantityInput.value);
 const totalPrice = priceOfProduct * quantity;
 totalPriceDisplay.textContent = totalPrice.toFixed(2);
 }
+//Creates a function to display the message based on what was ordered
+function placeOrder() {
+    const productDropdown = document.getElementById('product');
+    const quantityInput = document.getElementById('quantity');
+    const orderSummary = document.getElementById('orderSummary');
+  //Grabs the order
+    const selectedProduct = productDropdown.selectedOptions[0].text;
+    const quantity = parseInt(quantityInput.value) || 0;
+    const totalPrice = document.getElementById('totalPrice').textContent;
+  //Displays message
+    const summary = `You ordered ${quantity} of ${selectedProduct} totaling to ${totalPrice}`;
 
+    orderSummary.textContent = summary;
+  }
 //Dynamically updates the page
 document.getElementById('product').addEventListener('change',calculateTotal);
 document.getElementById('quantity').addEventListener('input', calculateTotal);
+document.querySelector('button').addEventListener('click', placeOrder);
